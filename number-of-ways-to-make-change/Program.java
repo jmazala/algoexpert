43c8d3429fdaeff31a7a7e9cd0a55b642fe7e4af
Jonathan Mazala <mazala.2@osu.edu>
2020-04-29 14:35:24 -0600

    number of ways to make change

---
+++ number-of-ways-to-make-change/Program.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+class Program {
+  public static int numberOfWaysToMakeChange(int n, int[] denoms) {
+    if (denoms.length == 0) {
+      return 0;
+    }
+
+    if (denoms.length == 1) {
+      return n % denoms[0] == 0 ? 1 : 0;
+    }
+
+    int[] dp = new int[n + 1];
+
+    dp[0] = 1; // only 1 way to make 0
+    Arrays.sort(denoms);
+
+    for (int coin : denoms) {
+      for (int i = 1; i <= n; i++) {
+        if (coin > n) {
+          break;
+        }
+
+        if (coin > i) {
+          continue;
+        }
+
+        int missing = i - coin;
+        dp[i] += dp[missing];
+      }
+    }
+
+    return dp[n];
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Program.numberOfWaysToMakeChange(6, new int[] { 1, 5 })); // 2
+    /*
+    1 - 16 pennies
+    2 - 11 pennies 1 nickel
+    3 - 6 pennies 2 nickels
+    4 - 6 pennies 1 dime
+    5 - 1 penny 3 nickels
+    6 - 1 penny 1 nickel 1 dime
+    */
+    System.out.println(Program.numberOfWaysToMakeChange(16, new int[] { 1, 5, 10, 25 })); // 6
+    System.out.println(Program.numberOfWaysToMakeChange(10, new int[] { 1, 5, 10, 25 })); // 4
+  }
+}
