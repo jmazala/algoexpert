16a519db1e8cf63e0f84a7df61f2c85f67fa0317
Jonathan Mazala <mazala.2@osu.edu>
2020-04-29 21:34:55 -0600

    O(n) time and O(1) space for single-cycle-check

--- single-cycle-check/index.js
+++ single-cycle-check/index.js
@@ -1,18 +1,6 @@
+//O(n) time and O(1) space
 function hasSingleCycle(array) {
-  const edges = Array(array.length);
-  for (let i = 0; i < array.length; i++) {
-    next = i + array[i];
-    while (next < 0) {
-      next = array.length + next;
-    }
-
-    if (next >= array.length) {
-      next = next % array.length;
-    }
-    edges[i] = next;
-  }
-
-  for (let start = 0; start < edges.length; start++) {
+  for (let start = 0; start < array.length; start++) {
     const complete = helper(start);
     if (complete) {
       return true;
@@ -23,16 +11,24 @@ function hasSingleCycle(array) {
 
   function helper(start) {
     const original = start;
-    const visited = new Set();
+    let steps = array.length;
 
-    let steps = edges.length;
     while (steps) {
-      if (visited.has(start)) {
+      let next = start + array[start];
+
+      while (next < 0) {
+        next = array.length + next;
+      }
+
+      if (next >= array.length) {
+        next = next % array.length;
+      }
+
+      if (next === start || steps > 1 && next === original) {
         return false;
       }
 
-      visited.add(start);
-      start = edges[start];
+      start = next;
       steps--;
     }
 
@@ -40,11 +36,53 @@ function hasSingleCycle(array) {
   }
 }
 
-// Do not edit the line below.
-exports.hasSingleCycle = hasSingleCycle;
+//O(n) time and O(n) space
+// function hasSingleCycle(array) {
+//   for (let start = 0; start < array.length; start++) {
+//     const complete = helper(start);
+//     if (complete) {
+//       return true;
+//     }
+//   }
+
+//   return false;
+
+//   function helper(start) {
+//     const original = start;
+//     const visited = new Set();
+//     let steps = array.length;
+
+//     while (steps) {
+//       if (visited.has(start)) {
+//         return false;
+//       }
+
+//       visited.add(start);
+      
+//       let next = start + array[start];
+
+//       while (next < 0) {
+//         next = array.length + next;
+//       }
+
+//       if (next >= array.length) {
+//         next = next % array.length;
+//       }
+
+//       start = next;
+//       steps--;
+//     }
+
+//     return start === original;
+//   }
+// }
+
+// // Do not edit the line below.
+// exports.hasSingleCycle = hasSingleCycle;
 
 console.log(hasSingleCycle([2, 3, 1, -4, -4, 2])); //true
 console.log(hasSingleCycle([2, 2, -1])); //true
 console.log(hasSingleCycle([2, 2, 2])); //true
 console.log(hasSingleCycle([0, 1, 1, 1, 1])); //false
 console.log(hasSingleCycle([1, 2, 3, 4, -2, 3, 7, 8, -26])); //true
+console.log(hasSingleCycle([1, -1, 1, -1])); //false;
