57172f60b1a74726caef8066fef08f993e0bfa04
Jonathan Mazala <mazala.2@osu.edu>
2020-05-01 15:38:08 -0600

    balanced brackets

---
+++ balanced-brackets/Program.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Program {
+  public static boolean balancedBrackets(String str) {
+    Stack<Character> stack = new Stack<>();
+    for (char c : str.toCharArray()) {
+      if (c == '(' || c == '{' || c == '[') {
+        stack.push(c);
+      } else if (c == ')') {
+        if (stack.isEmpty() || stack.pop() != '(') {
+          return false;
+        }
+      } else if (c == '}') {
+        if (stack.isEmpty() || stack.pop() != '{') {
+          return false;
+        }
+      } else if (c == ']') {
+        if (stack.isEmpty() || stack.pop() != '[') {
+          return false;
+        }
+      }
+    }
+
+    return stack.isEmpty();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Program.balancedBrackets("([])(){}(())()()"));
+  }
+}
