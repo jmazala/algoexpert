f4d367f1154983458d2330b32530ef78f8e5ee15
Jonathan Mazala <mazala.2@osu.edu>
2020-05-04 13:03:58 -0600

    suffix trie construction

---
+++ suffix-trie-construction/Program.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Program {
+  // Do not edit the class below except for the
+  // populateSuffixTrieFrom and contains methods.
+  // Feel free to add new properties and methods
+  // to the class.
+  static class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+  }
+
+  static class SuffixTrie {
+    TrieNode root = new TrieNode();
+    char endSymbol = '*';
+
+    public SuffixTrie(String str) {
+      populateSuffixTrieFrom(str);
+    }
+
+    public void populateSuffixTrieFrom(String str) {
+      for (int i = 0; i < str.length(); i++) {
+        insertSubstring(i, str);
+      }
+    }
+
+    public void insertSubstring(int i, String str) {
+      TrieNode temp = root;
+      while (i < str.length()) {
+        char c = str.charAt(i);
+        if (!temp.children.containsKey(c)) {
+          temp.children.put(c, new TrieNode());
+        }
+
+        temp = temp.children.get(c);
+        i++;
+      }
+
+      temp.children.put(endSymbol, null);
+    }
+
+    public boolean contains(String str) {
+      TrieNode temp = root;
+
+      for (char c : str.toCharArray()) {
+        temp = temp.children.get(c);
+        if (temp == null) {
+          return false;
+        }
+      }
+
+      return temp.children.containsKey(endSymbol);
+    }
+  }
+}
