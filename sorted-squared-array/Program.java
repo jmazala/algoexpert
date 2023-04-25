5dede38f85abd04ec387458164d7c3c83a22c8ab
Jonathan Mazala <mazala.2@osu.edu>
2023-04-25 16:10:11 -0700

    spaces

--- sorted-squared-array/Program.java
+++ sorted-squared-array/Program.java
@@ -2,30 +2,34 @@ import java.util.*;
 
 class Program {
 
-    /*
-     * O(n) for array iterate
-     * O(n log n) for sort
-     * O(n) for iterate
-     * = O(n log n) solution
-     * O(1) for space (unless Arrays.sort isn't)
-     */
-    public static int[] sortedSquaredArray(int[] input) {
-        for (int i = 0; i < input.length; i++) {
-            if (input[i] < 0) {
-                input[i] *= -1;
-            }
-        }
-
-        Arrays.sort(input);
-
-        for (int i = 0; i < input.length; i++) {
-            input[i] *= input[i];
-        }
-
-        return input;
+  /*
+   * O(n) for array iterate
+   * O(n log n) for sort
+   * O(n) for iterate
+   * = O(n log n) solution
+   * O(1) for space (unless Arrays.sort isn't)
+   */
+  public static int[] sortedSquaredArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] < 0) {
+        array[i] *= -1;
+      }
     }
 
-    public static void main(String[] args) {
-        System.out.println(Program.sortedSquaredArray(new int[] { 1, 2, 3, 5, 6, 8, 9 }));
+    Arrays.sort(array);
+
+    for (int i = 0; i < array.length; i++) {
+      array[i] *= array[i];
     }
+
+    return array;
+  }
+
+  /*
+   * we have to sort it no matter what, we could do a mergesort
+   */
+
+  public static void main(String[] args) {
+    System.out.println(Program.sortedSquaredArray(new int[] { 1, 2, 3, 5, 6, 8, 9 }));
+  }
 }
