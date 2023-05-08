0cbecffdcb89fd5a76a90fa5770ef1ed7c8ad0e8
Jonathan Mazala <mazala.2@osu.edu>
2023-05-08 14:15:44 -0700

    caesar

--- caesar-cipher-encryptor/CaesarCipherEncryptor.java
+++ caesar-cipher-encryptor/CaesarCipherEncryptor.java
@@ -2,21 +2,28 @@ class CaesarCipherEncryptor {
   public static String caesarCypherEncryptor(String str, int key) {
     char[] result = new char[str.length()];
     key = key % 26;
-    String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+    /*
+     * can convert between chars and ints pretty easily in Java
+     * Convert current char to int
+     * Add key, do modulo to keep in bounds
+     * Convert back to char and add to result array
+     */
     for (int i = 0; i < str.length(); i++) {
-      result[i] = getNextLetter(str.charAt(i), key, alphabet);
+      int c = (int) (str.charAt(i) - 'a');
+      c = (c + key) % 26;
+      result[i] = (char) (c + 'a');
     }
 
     return new String(result);
   }
 
-  private static char getNextLetter(char letter, int key, String alphabet) {
-    int code = alphabet.indexOf(letter) + key;
-    if (code > 25) {
-      code = -1 + code % 25;
-    }
-
-    return alphabet.charAt(code);
+  public static void main(String[] args) {
+    System.out.println(CaesarCipherEncryptor.caesarCypherEncryptor("", 2)); // ""
+    System.out.println(CaesarCipherEncryptor.caesarCypherEncryptor("a", 0)); // "a"
+    System.out.println(CaesarCipherEncryptor.caesarCypherEncryptor("a", 1)); // "b"
+    System.out.println(CaesarCipherEncryptor.caesarCypherEncryptor("a", 26)); // "a"
+    System.out.println(CaesarCipherEncryptor.caesarCypherEncryptor("a", 27)); // "b"
+    System.out.println(CaesarCipherEncryptor.caesarCypherEncryptor("xyz", 2)); // "zab"
   }
 }
--- caesar-cipher-encryptor/index.js
+++ caesar-cipher-encryptor/index.js
@@ -1,23 +1,24 @@
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+// O(n) time and O(n) space
 function caesarCipherEncryptor(string, key) {
-  const result = Array(string.length);
-  key = key % 26;
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let output = '';
 
   for (let i = 0; i < string.length; i++) {
-    result[i] = nextLetter(string[i]);
+    const nextLetterIndex = (ALPHABET.indexOf(string[i]) + key) % 26;
+    output += ALPHABET[nextLetterIndex];
   }
 
-  return result.join('');
-
-  function nextLetter(c) {
-    let nextLetterIndex = alphabet.indexOf(c) + key;
-    if (nextLetterIndex > 25) {
-      nextLetterIndex = -1 + (nextLetterIndex % 25);
-    }
-
-    return alphabet[nextLetterIndex];
-  }
+  return output;
 }
 
 // Do not edit the line below.
 exports.caesarCipherEncryptor = caesarCipherEncryptor;
+
+console.log(caesarCipherEncryptor('', 2)); // ''
+console.log(caesarCipherEncryptor('a', 0)); // 'a'
+console.log(caesarCipherEncryptor('a', 1)); // 'b'
+console.log(caesarCipherEncryptor('a', 26)); // 'a'
+console.log(caesarCipherEncryptor('a', 27)); // 'b'
+console.log(caesarCipherEncryptor('xyz', 2)); // zab
+
