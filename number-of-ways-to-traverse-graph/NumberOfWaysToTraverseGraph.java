94b31342994897256d00308594fd65e22251eb41
Jonathan Mazala <mazala.2@osu.edu>
2023-05-15 20:39:29 -0700

    problem

---
+++ number-of-ways-to-traverse-graph/NumberOfWaysToTraverseGraph.java
@@ -0,0 +1,22 @@
+// https://www.algoexpert.io/questions/number-of-ways-to-traverse-graph
+
+import java.util.*;
+
+class Program {
+
+  public int numberOfWaysToTraverseGraph(int width, int height) {
+    int[][] dp = new int[height][width];
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (i == 0 || j == 0) {
+          dp[i][j] = 1;
+          continue;
+        }
+
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+
+    return dp[height - 1][width - 1];
+  }
+}
