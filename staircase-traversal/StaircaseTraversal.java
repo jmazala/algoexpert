e87e519312f223ba06b7d1e41a2643025382d4cf
Jonathan Mazala <mazala.2@osu.edu>
2023-05-24 21:07:47 -0700

    probs

---
+++ staircase-traversal/StaircaseTraversal.java
@@ -0,0 +1,28 @@
+// https://www.algoexpert.io/questions/staircase-traversal
+
+import java.util.*;
+
+class StaircaseTraversal {
+
+  public static int staircaseTraversal(int height, int maxSteps) {
+    int[] dp = new int[height + 1];
+    dp[0] = 1;
+
+    for (int i = 1; i <= height; i++) {
+      for (int distance = 1; distance <= maxSteps; distance++) {
+        int prev = i - distance;
+        if (prev < 0) {
+          continue;
+        }
+
+        dp[i] += dp[prev];
+      }
+    }
+
+    return dp[height];
+  }
+
+  public static void main(String[] args) {
+    System.out.println(StaircaseTraversal.staircaseTraversal(4, 2)); // 5
+  }
+}
