ca95310808a9fd9b2b09e1f8d0f958879a5cefd8
Jonathan Mazala <mazala.2@osu.edu>
2023-05-29 06:08:05 -0700

    min number of coins is really a BFS problem

--- min-number-of-coins-for-change/MinNumberOfCoinsForChange.java
+++ min-number-of-coins-for-change/MinNumberOfCoinsForChange.java
@@ -32,9 +32,62 @@ public class MinNumberOfCoinsForChange {
     return dp[n] == null ? -1 : dp[n];
   }
 
+  /*
+   * Use a BFS structure to search until we find n.
+   * Because it's BFS, we will always use the least number of steps (coins) to
+   * arrive
+   * at n.
+   * 
+   * Optimized further by filtering the denoms array twice
+   * 
+   * 1 - once at inception for any denoms > n
+   * 2 - When BFSing, don't bother queueing next items greater than n
+   */
+  public static int minNumberOfCoinsForChange2(int n, int[] denoms) {
+    if (n == 0) {
+      return 0;
+    }
+
+    denoms = Arrays.stream(denoms).filter(x -> x <= n).toArray();
+    Arrays.sort(denoms);
+
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(0);
+    int[] dp = new int[n + 1];
+    Arrays.fill(dp, -1);
+    dp[0] = 0;
+
+    while (!queue.isEmpty()) {
+      int queueSize = queue.size();
+      for (int i = 0; i < queueSize; i++) {
+        int amount = queue.remove();
+        int coins = dp[amount];
+
+        for (int denom : Arrays.stream(denoms).filter(x -> (x + amount) <= n).toArray()) {
+          int nextAmount = amount + denom;
+
+          if (dp[nextAmount] != -1) {
+            continue;
+          }
+
+          int nextCoins = coins + 1;
+
+          if (nextAmount == n) {
+            return nextCoins;
+          }
+
+          dp[nextAmount] = nextCoins;
+          queue.add(nextAmount);
+        }
+      }
+    }
+
+    return -1;
+  }
+
   public static void main(String[] args) {
-    System.out.println(minNumberOfCoinsForChange(7, new int[] { 2, 4 })); // -1
-    System.out.println(minNumberOfCoinsForChange(7, new int[] { 1, 5, 10 })); // 3
-    System.out.println(minNumberOfCoinsForChange(14, new int[] { 1, 5, 10 })); // 5
+    System.out.println(minNumberOfCoinsForChange2(7, new int[] { 2, 4 })); // -1
+    System.out.println(minNumberOfCoinsForChange2(7, new int[] { 1, 5, 10 })); // 3
+    System.out.println(minNumberOfCoinsForChange2(14, new int[] { 1, 5, 10 })); // 5
   }
 }
