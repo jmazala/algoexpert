c3fe7ae8053e0f60113f8ceb687d975d72aac903
Jonathan Mazala <mazala.2@osu.edu>
2023-06-23 13:30:17 -0700

    quick sort in python

--- quick-sort/main.py
+++ quick-sort/main.py
@@ -1,23 +1,45 @@
+# https://www.algoexpert.io/questions/quick-sort
+
+
 def quickSort(array):
-  helper(array, 0, len(array) - 1)
-  return array
+    helper(array, 0, len(array) - 1)
+    return array
+
 
 def helper(array, low, high):
-  if (low >= high):
+    if low >= high:
+        return
+
+    partitionIndex = partition(array, low, high)
+    helper(array, low, partitionIndex - 1)
+    helper(array, partitionIndex + 1, high)
     return
 
-  partitionIndex = partition(array, low, high)
-  helper(array, low, partitionIndex - 1)
-  helper(array, partitionIndex + 1, high)
-  return
 
 def partition(array, low, high):
-  pivot = array[high]
-  i = low
-  for j in range(low, high):
-    if array[j] < pivot:
-      [array[i], array[j]] = [array[j], array[i]]
-      i += 1
+    pivot = array[low]
+    i = low + 1
+    j = high
 
-  [array[i], array[high]] = [array[high], array[i]]
-  return i
+    while i <= j:
+        if array[i] > pivot and array[j] < pivot:
+            swap(array, i, j)
+            i += 1
+            j -= 1
+            continue
+        if array[i] <= pivot:
+            i += 1
+        if array[j] >= pivot:
+            j -= 1
+
+    swap(array, low, j)
+    return j
+
+
+def swap(array, i, j):
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+
+
+print(quickSort([8, 11, 2, 3, 1, 4, 9, 15, 15, 13, 6]))
